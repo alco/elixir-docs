f81825114d4fb153983d3c6e27eeedebc7357e66
Alexei Sholik <alcosholik@gmail.com>
2015-04-19 14:33:27 +0300

    Update generated docs

--- conf.py
+++ conf.py
@@ -187,14 +187,14 @@ htmlhelp_basename = 'Elixirdoc'
 # -- Options for LaTeX output ---------------------------------------------
 
 latex_elements = {
-# The paper size ('letterpaper' or 'a4paper').
-#'papersize': 'letterpaper',
+  # The paper size ('letterpaper' or 'a4paper').
+  #'papersize': 'letterpaper',
 
-# The font size ('10pt', '11pt' or '12pt').
-#'pointsize': '10pt',
+  # The font size ('10pt', '11pt' or '12pt').
+  #'pointsize': '10pt',
 
-# Additional stuff for the LaTeX preamble.
-#'preamble': '',
+  # Additional stuff for the LaTeX preamble.
+  'preamble': '\setcounter{tocdepth}{2}'
 }
 
 # Grouping the document tree into LaTeX files. List of tuples
